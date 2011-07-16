4a89c566214f6fd3a581e1ec3c385a82588be822
tranduyminhman <tranduyminhman@2b12dacf-5720-f21c-a8c3-10721cf620b2>
2011-07-16 17:37:51 +0000

    init gappHost

---
+++ source/gappHost/protected/config/main.php
@@ -0,0 +1,91 @@
+<?php
+
+// uncomment the following to define a path alias
+// Yii::setPathOfAlias('local','path/to/local-folder');
+// This is the main Web application configuration. Any writable
+// CWebApplication properties can be configured here.
+return array(
+    'basePath' => dirname(__FILE__) . DIRECTORY_SEPARATOR . '..',
+    'name' => 'GAPP',
+    // preloading 'log' component
+    'preload' => array('log'),
+    // autoloading model and component classes
+    'import' => array(
+        'application.models.*',
+        'application.components.*',
+    ),
+    'modules' => array(
+        // uncomment the following to enable the Gii tool
+
+        'gii' => array(
+            'class' => 'system.gii.GiiModule',
+            'password' => '123456',
+            // If removed, Gii defaults to localhost only. Edit carefully to taste.
+            //'ipFilters' => array('127.0.0.1', '::1'),
+        ),
+    ),
+    // application components
+    'components' => array(
+        'user' => array(
+            // enable cookie-based authentication
+            'allowAutoLogin' => true,
+        ),
+        // uncomment the following to enable URLs in path-format
+        'urlManager' => array(
+            'urlFormat' => 'path',
+            'rules' => array(                
+                // REST patterns
+                array('api/list', 'pattern' => 'api/<model:\w+>', 'verb' => 'GET'),    
+                array('api/view', 'pattern' => 'api/<model:\w+>/<category:\w+>/<id:\d+>', 'verb' => 'GET'),
+                array('api/view', 'pattern' => 'api/<model:\w+>/<id:\d+>', 'verb' => 'GET'),                                  
+                array('api/update', 'pattern' => 'api/<model:\w+>/<id:\d+>', 'verb' => 'PUT'),
+                array('api/delete', 'pattern' => 'api/<model:\w+>/<id:\d+>', 'verb' => 'DELETE'),
+                array('api/create', 'pattern' => 'api/<model:\w+>', 'verb' => 'POST'),
+                // Other controllers
+				'<controller:\w+>/<id:\d+>'=>'<controller>/view',
+                '<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
+                '<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
+            ),
+            'showScriptName' => false, 
+        ),
+        /*
+          'db' => array(
+          'connectionString' => 'sqlite:' . dirname(__FILE__) . '/../data/testdrive.db',
+          ),
+         */
+        // uncomment the following to use a MySQL database
+
+        'db' => array(
+            'connectionString' => 'mysql:host=sql110.freevnn.com;dbname=freev_8525956_gapp',
+            'emulatePrepare' => true,
+            'username' => 'freev_8525956',
+            'password' => 'zxcvbnm',
+            'charset' => 'utf8',
+        ),
+        'errorHandler' => array(
+            // use 'site/error' action to display errors
+            'errorAction' => 'site/error',
+        ),
+        'log' => array(
+            'class' => 'CLogRouter',
+            'routes' => array(
+                array(
+                    'class' => 'CFileLogRoute',
+                    'levels' => 'error, warning',
+                ),
+            // uncomment the following to show log messages on web pages
+            /*
+              array(
+              'class'=>'CWebLogRoute',
+              ),
+             */
+            ),
+        ),
+    ),
+    // application-level parameters that can be accessed
+    // using Yii::app()->params['paramName']
+    'params' => array(
+        // this is used in contact page
+        'adminEmail' => 'webmaster@example.com',
+    ),
+);
