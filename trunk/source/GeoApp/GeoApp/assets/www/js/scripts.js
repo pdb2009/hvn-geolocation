a0b05657a14ee6d5c7dac393ae0c22b93f02d7cf
tranduyminhman <tranduyminhman@2b12dacf-5720-f21c-a8c3-10721cf620b2>
2011-07-27 10:27:38 +0000

    update

--- trunk/source/GeoApp/GeoApp/assets/www/js/scripts.js
+++ trunk/source/GeoApp/GeoApp/assets/www/js/scripts.js
@@ -1,20 +1,53 @@
-$(document).ready(function(){
-	/*$.ajax({
-		url: 'http://thiencong.com/gapp/index.php/api/category',
-		dataType: 'json',
-		success: function(data) {
-			var lstData = $('#lstData');
-			var str = '';
-			$.each(data, function(key, val) {	
-				str += '<li><a data-identity="cat' + val.id + '" data-url="?catid=' + val.id + '" href="javascript:void(0);"><img src="http://thiencong.com/gapp/images/categoy/' + val.icon + '" /><h3>' + val.title + '</h3><p>' + val.description + '</p></a><a href="#">' + val.title + '</a></li>';												
-			});
-			if(str != '') {
-				lstData.html('<ul>' + str + '</ul>');
-				lstData.listview("refresh");
-			}
+(function($) {
+	var methods = {
+		initGlobal : function() {
+			//TODO:
+			$.mobile.autoBackBtn = true;
 		},
-		error: function() {
+		initPage1 : function() {
+			var $page = $("#page1");
+			//TODO:
+			$page.find("a[href='#page2'],").click(function(){
+				$("#page2").data('demo', 'lee2');
+			});
+			$page.find("a[href='#page3'],").click(function(){
+				$("#page3").data('demo', 'lee3');
+			});
+		},
+		initPage2 : function() {
+			var $page = $("#page2");
+			//TODO:
+			$page.bind('pagebeforeshow', function(){
+				var $demo = $page.data('demo');
+				$page.find(".content").text($demo);
+			});
+		},
+		initPage3 : function() {
+			var $page = $("#page3");
+			//TODO:
+			$page.bind('pageshow', function(){
+				var $demo = $page.data('demo');
+				$page.find(".content").text($demo);
+			});
+		},
+		initAll : function() {
+			$().initApp("initGlobal");
+			$().initApp("initPage1");
+			$().initApp("initPage2");
+			$().initApp("initPage3");
 		}
-	});*/
-	$.mobile.autoBackBtn = true;
+	}
+	$.fn.initApp = function(method) {
+		// Method calling logic
+		if ( methods[method] ) {
+			return methods[ method ].apply( this, Array.prototype.slice.call( arguments, 1 ));
+		} else if ( typeof method === 'object' || ! method ) {
+			return methods.initAll.apply( this, arguments );
+		} else {
+			$.error( 'Method ' + method + ' does not exist' );
+		}
+	}
+})(jQuery);
+$(document).ready(function(){
+	$().initApp();
 });
